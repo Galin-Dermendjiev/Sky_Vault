77682392b66a6f8a47b735b238150caedddd5de6
Galin <sao4449@abv.bg>
2025-12-07 22:45:26 +0200

    Implement sorting files

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,4 +1,42 @@
-export default function Sort(){
-    return <>
-    </>
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { sortTypes } from "@/constants";
+import { usePathname, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
+
+export default function Sort() {
+  const router = useRouter();
+  const path = usePathname();
+  const searchParams = useSearchParams();
+
+  function handleSort(value: string) {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("sort", value);
+    router.push(`${path}?${params.toString()}`);
+  }
+  return (
+    <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+      <SelectTrigger className="sort-select">
+        <SelectValue placeholder={sortTypes[0].value} />
+      </SelectTrigger>
+      <SelectContent className="sort-select-content">
+        {sortTypes.map((sort) => (
+          <SelectItem
+            key={sort.label}
+            value={sort.value}
+            className="shad-select-item"
+          >
+            {sort.label}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  );
 }
